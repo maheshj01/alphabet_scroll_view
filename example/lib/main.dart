f9a7d90906f47b8e62b79d736f0ef71792feb17e
mahesh jamdade <maheshmn121@gmail.com>
2023-07-13 22:58:55 -0400

    create a basic api

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,205 +1,247 @@
-import 'package:flutter/material.dart';
-
 import 'package:alphabet_scroll_view/alphabet_scroll_view.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Alphabet ScrollView Demo',
+        themeMode: ThemeMode.light,
+        darkTheme: ThemeData.dark(
+          useMaterial3: true,
+        ),
+        home: const HomePage());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {}
-
-  List<String> list = [
-    'angel',
-    'bubbles',
-    'shimmer',
-    'angelic',
-    'bubbly',
-    'glimmer',
-    'baby',
-    'pink',
-    'little',
-    'butterfly',
-    'sparkly',
-    'doll',
-    'sweet',
-    'sparkles',
-    'dolly',
-    'sweetie',
-    'sprinkles',
-    'lolly',
-    'princess',
-    'fairy',
-    'honey',
-    'snowflake',
-    'pretty',
-    'sugar',
-    'cherub',
-    'lovely',
-    'blossom',
-    'Ecophobia',
-    'Hippophobia',
-    'Scolionophobia',
-    'Ergophobia',
-    'Musophobia',
-    'Zemmiphobia',
-    'Geliophobia',
-    'Tachophobia',
-    'Hadephobia',
-    'Radiophobia',
-    'Turbo Slayer',
-    'Cryptic Hatter',
-    'Crash TV',
-    'Blue Defender',
-    'Toxic Headshot',
-    'Iron Merc',
-    'Steel Titan',
-    'Stealthed Defender',
-    'Blaze Assault',
-    'Venom Fate',
-    'Dark Carnage',
-    'Fatal Destiny',
-    'Ultimate Beast',
-    'Masked Titan',
-    'Frozen Gunner',
-    'Bandalls',
-    'Wattlexp',
-    'Sweetiele',
-    'HyperYauFarer',
-    'Editussion',
-    'Experthead',
-    'Flamesbria',
-    'HeroAnhart',
-    'Liveltekah',
-    'Linguss',
-    'Interestec',
-    'FuzzySpuffy',
-    'Monsterup',
-    'MilkA1Baby',
-    'LovesBoost',
-    'Edgymnerch',
-    'Ortspoon',
-    'Oranolio',
-    'OneMama',
-    'Dravenfact',
-    'Reallychel',
-    'Reakefit',
-    'Popularkiya',
-    'Breacche',
-    'Blikimore',
-    'StoneWellForever',
-    'Simmson',
-    'BrightHulk',
-    'Bootecia',
-    'Spuffyffet',
-    'Rozalthiric',
-    'Bookman'
-  ];
-
-  int selectedIndex = 0;
+class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: AlphabetScrollView(
-              list: list.map((e) => AlphaModel(e)).toList(),
-              // isAlphabetsFiltered: false,
-              alignment: LetterAlignment.right,
-              itemExtent: 50,
-              unselectedTextStyle: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-                color: Colors.black
-              ),
-              selectedTextStyle: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red
-              ),
-              overlayWidget: (value) => Stack(
-                alignment: Alignment.center,
-                children: [
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      // color: Theme.of(context).primaryColor,
-                    ),
-                    alignment: Alignment.center,
-                    child: Text(
-                      '$value'.toUpperCase(),
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-              itemBuilder: (_, k, id) {
-                return Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: ListTile(
-                    title: Text('$id'),
-                    subtitle: Text('Secondary text'),
-                    leading: Icon(Icons.person),
-                    trailing: Radio<bool>(
-                      value: false,
-                      groupValue: selectedIndex != k,
-                      onChanged: (value) {
-                        setState(() {
-                          selectedIndex = k;
-                        });
-                      },
-                    ),
-                  ),
-                );
-              },
+      appBar: AppBar(title: const Text('Alphabet Scroll View Demo')),
+      body: AlphabetScrollView(
+        list: list,
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (BuildContext context, int index, String letter) {
+          return Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: ListTile(
+              title: Text(list[index]),
+              subtitle: Text('Secondary text'),
+              leading: Icon(Icons.person),
             ),
-          )
-        ],
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-        ],
+          );
+        },
+        onLetterChanged: (x) {},
+        overlayWidget: (value) => Stack(
+          alignment: Alignment.center,
+          children: [
+            Icon(
+              Icons.star,
+              size: 50,
+              color: Colors.red,
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                // color: Theme.of(context).primaryColor,
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                '$value'.toUpperCase(),
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+List<String> list = [
+  'angel',
+  'bubbles',
+  'shimmer',
+  'angelic',
+  'bubbly',
+  'glimmer',
+  'baby',
+  'pink',
+  'little',
+  'butterfly',
+  'sparkly',
+  'doll',
+  'sweet',
+  'sparkles',
+  'dolly',
+  'sweetie',
+  'sprinkles',
+  'lolly',
+  'princess',
+  'fairy',
+  'honey',
+  'snowflake',
+  'pretty',
+  'sugar',
+  'cherub',
+  'lovely',
+  'blossom',
+  'Ecophobia',
+  'Hippophobia',
+  'Scolionophobia',
+  'Ergophobia',
+  'Musophobia',
+  'Zemmiphobia',
+  'Geliophobia',
+  'Tachophobia',
+  'Hadephobia',
+  'Radiophobia',
+  'Turbo Slayer',
+  'Cryptic Hatter',
+  'Crash TV',
+  'Blue Defender',
+  'Toxic Headshot',
+  'Iron Merc',
+  'Steel Titan',
+  'Stealthed Defender',
+  'Blaze Assault',
+  'Venom Fate',
+  'Dark Carnage',
+  'Fatal Destiny',
+  'Ultimate Beast',
+  'Masked Titan',
+  'Frozen Gunner',
+  'Bandalls',
+  'Wattlexp',
+  'Sweetiele',
+  'HyperYauFarer',
+  'Editussion',
+  'Experthead',
+  'Flamesbria',
+  'HeroAnhart',
+  'Liveltekah',
+  'Linguss',
+  'Interestec',
+  'FuzzySpuffy',
+  'Monsterup',
+  'MilkA1Baby',
+  'LovesBoost',
+  'Edgymnerch',
+  'Ortspoon',
+  'Oranolio',
+  'OneMama',
+  'Dravenfact',
+  'Reallychel',
+  'Reakefit',
+  'Popularkiya',
+  'Breacche',
+  'Blikimore',
+  'StoneWellForever',
+  'Simmson',
+  'BrightHulk',
+  'Bootecia',
+  'Spuffyffet',
+  'Rozalthiric',
+  'Bookman'
+];
+
+//   int selectedIndex = 0;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Column(
+//         children: [
+//           Expanded(
+//             child: AlphabetScrollView(
+//               list: list.map((e) => AlphaModel(e)).toList(),
+//               // isAlphabetsFiltered: false,
+//               alignment: LetterAlignment.right,
+//               itemExtent: 50,
+//               unselectedTextStyle: TextStyle(
+//                 fontSize: 18,
+//                 fontWeight: FontWeight.normal,
+//                 color: Colors.black
+//               ),
+//               selectedTextStyle: TextStyle(
+//                   fontSize: 20,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.red
+//               ),
+//               overlayWidget: (value) => Stack(
+//                 alignment: Alignment.center,
+//                 children: [
+//                   Icon(
+//                     Icons.star,
+//                     size: 50,
+//                     color: Colors.red,
+//                   ),
+//                   Container(
+//                     height: 50,
+//                     width: 50,
+//                     decoration: BoxDecoration(
+//                       shape: BoxShape.circle,
+//                       // color: Theme.of(context).primaryColor,
+//                     ),
+//                     alignment: Alignment.center,
+//                     child: Text(
+//                       '$value'.toUpperCase(),
+//                       style: TextStyle(fontSize: 18, color: Colors.white),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               itemBuilder: (_, k, id) {
+//                 return Padding(
+//                   padding: const EdgeInsets.only(right: 20),
+//                   child: ListTile(
+//                     title: Text('$id'),
+//                     subtitle: Text('Secondary text'),
+//                     leading: Icon(Icons.person),
+//                     trailing: Radio<bool>(
+//                       value: false,
+//                       groupValue: selectedIndex != k,
+//                       onChanged: (value) {
+//                         setState(() {
+//                           selectedIndex = k;
+//                         });
+//                       },
+//                     ),
+//                   ),
+//                 );
+//               },
+//             ),
+//           )
+//         ],
+//       ),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           FloatingActionButton(
+//             onPressed: _incrementCounter,
+//             tooltip: 'Increment',
+//             child: Icon(Icons.add),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- lib/src/alphabet_scroll.dart
+++ lib/src/alphabet_scroll.dart
@@ -1,49 +1,10 @@
-import 'dart:async';
-import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Alphabet ScrollView Demo',
-        themeMode: ThemeMode.light,
-        darkTheme: ThemeData.dark(
-          useMaterial3: true,
-        ),
-        home: const HomePage());
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePage();
-}
-
-class _HomePage extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(height: 600, child: AlphabetScrollView()),
-      ],
-    );
-  }
-}
-
 enum LetterAlignment { left, right }
 
-class AlphabetScrollView extends StatefulWidget {
-  // final List<AlphaModel> list;
+class AlphabetScrollView<T> extends StatefulWidget {
+  final List<T> list;
 
   // final double itemExtent;
 
@@ -56,54 +17,117 @@ class AlphabetScrollView extends StatefulWidget {
   // final TextStyle selectedTextStyle;
 
   // final TextStyle unselectedTextStyle;
-
+  final Function(String)? onLetterChanged;
   // /// The itemBuilder must return a non-null widget and the third paramter id specifies
   // /// the string mapped to this widget from the ```[list]``` passed.
+  final ScrollPhysics physics;
 
-  // Widget Function(BuildContext, int, String) itemBuilder;
+  final ScrollController? controller;
 
-  AlphabetScrollView({
-    Key? key,
-    // required this.list,
-    // this.alignment = LetterAlignment.right,
-    // this.isAlphabetsFiltered = true,
-    this.overlayWidget,
-    // required this.selectedTextStyle,
-    // required this.unselectedTextStyle,
-    // this.itemExtent = 40,
-    // required this.itemBuilder
-  }) : super(key: key);
+  /// context, index, letter
+  Widget Function(BuildContext, int, String) itemBuilder;
+
+  AlphabetScrollView(
+      {Key? key,
+      required this.list,
+      // this.alignment = LetterAlignment.right,
+      // this.isAlphabetsFiltered = true,
+      this.overlayWidget,
+      this.controller,
+      // required this.selectedTextStyle,
+      // required this.unselectedTextStyle,
+      // this.itemExtent = 40,
+      this.physics = const AlwaysScrollableScrollPhysics(),
+      this.onLetterChanged,
+      required this.itemBuilder})
+      : super(key: key);
 
   @override
   State<AlphabetScrollView> createState() => _AlphabetScrollViewState();
 }
 
 class _AlphabetScrollViewState extends State<AlphabetScrollView> {
+  ScrollController? _scrollController;
+  int itemCount = 0;
+  @override
+  void initState() {
+    super.initState();
+    if (widget.controller == null) {
+      _scrollController = ScrollController();
+    } else {
+      _scrollController = widget.controller!;
+    }
+    itemCount = widget.list.length;
+  }
+
+  Map<String, double> letterToOffset = {};
+
+  /// Maps the letter to the offset of the item in the list
+  void calculateOffset<T>() {
+    for (int i = 0; i < widget.list.length; i++) {
+      final item = (widget.list as List<T>)[i];
+      final letter = item.toString().substring(0, 1).toLowerCase();
+      if (!letterToOffset.containsKey(letter)) {
+        letterToOffset[letter] = i.toDouble();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      _scrollController!.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _AlphabetScrollRenderObject(
-      alphabets,
-      onLetterSelected: (x) {
-        print(x);
-      },
-      
+    return Row(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            physics: widget.physics,
+            controller: _scrollController,
+            itemCount: itemCount,
+            itemBuilder: (context, index) {
+              return widget.itemBuilder(
+                  context, index, alphabets[index].toUpperCase());
+            },
+          ),
+        ),
+        _AlphabetScrollRenderObject(
+          alphabets,
+          onLetterChanged: (letter) {
+            final index = alphabets.indexOf(letter);
+            // if (index != -1) {
+            //   final randomOffset = Random().nextDouble() * itemCount;
+            //   _scrollController.animateTo(randomOffset,
+            //       duration: Duration(milliseconds: 300),
+            //       curve: Curves.bounceIn);
+            // }
+          },
+          overlayWidget: widget.overlayWidget,
+        ),
+      ],
     );
   }
 }
 
 class _AlphabetScrollRenderObject extends SingleChildRenderObjectWidget {
   final List<String> letters;
-  final Function(String)? onLetterSelected;
+  final Function(String)? onLetterChanged;
   final Widget Function(String)? overlayWidget;
 
-  const _AlphabetScrollRenderObject(this.letters, {this.onLetterSelected,this.overlayWidget});
+  const _AlphabetScrollRenderObject(this.letters,
+      {this.onLetterChanged, this.overlayWidget});
 
   @override
   RenderObject createRenderObject(BuildContext context) {
     return CustomAlphabetListViewRenderBox(
         letters: letters,
-        onLetterChanged: onLetterSelected,
-        overlayWidget:overlayWidget,
+        onLetterChanged: onLetterChanged,
+        overlayWidget: overlayWidget,
         alignment: LetterAlignment.left);
   }
 }
@@ -113,6 +137,7 @@ class CustomAlphabetListViewRenderBox extends RenderBox {
   final Function(String)? onLetterChanged;
   LetterAlignment alignment;
   final Widget Function(String)? overlayWidget;
+
   /// location for the listItem to be rendered
   List<Offset>? _itemOffsets;
   String? selectedLetter;
@@ -128,14 +153,18 @@ class CustomAlphabetListViewRenderBox extends RenderBox {
 
   @override
   void performLayout() {
-    size = constraints.biggest;
+    final biggest = constraints.biggest;
+    size = Size(20, biggest.height);
+
+    final dx = size.width - 20.0;
+    final startOffset = 100.0;
 
     /// Will spread the available height equally across letters
-    final dx = alignment == LetterAlignment.left ? 8.0 : size.width - 8.0;
-    final itemHeight = size.height / letters.length;
+    final itemHeight = (size.height - (2 * startOffset)) / letters.length;
+    print(itemHeight);
     _itemOffsets = List.generate(
       letters.length,
-      (index) => Offset(dx, itemHeight * index),
+      (index) => Offset(dx, itemHeight * index + startOffset),
     );
   }
 
@@ -169,28 +198,26 @@ class CustomAlphabetListViewRenderBox extends RenderBox {
       final itemHeight = size.height / letters.length;
       final itemOffset = _itemOffsets![i] + offset;
 
-     
       final itemRect = Rect.fromLTWH(
-        itemOffset.dx + itemHeight /2,
+        itemOffset.dx + itemHeight / 2,
         itemOffset.dy,
         itemHeight,
         itemHeight,
       );
-       if (isSelected) {
+      if (isSelected) {
         final squareRect = itemRect.deflate(4.0);
         context.canvas.drawRect(
-          squareRect,
-          Paint()
-            ..color = Colors.blue
-            ..style = PaintingStyle.fill
-        );
+            squareRect,
+            Paint()
+              ..color = Colors.blue
+              ..style = PaintingStyle.fill);
       }
       final textSpan =
           TextSpan(text: letters[i].toUpperCase(), style: textStyle);
       final textPainter = TextPainter(
         text: textSpan,
         textDirection: TextDirection.ltr,
-      )..layout(minWidth: 0, maxWidth: size.width);
+      )..layout(minWidth: 0, maxWidth: 16);
       textPainter.paint(context.canvas, itemOffset);
     }
   }
